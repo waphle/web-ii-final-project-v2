5658b496c3b658ec992a40b8459b298097c72708
Jeff Zhang <40436311+waphle@users.noreply.github.com>
2022-05-05 15:12:07 -0400

    Update app.js
    Added comments to backend app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// requires the places-routes and users-routes files to run
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
@@ -10,17 +11,19 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Functions for the header buttons
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // Allows button functions to get (import), post (export), patch (edit), and delete user cred. based on user feedback
 
   next();
 });
 
+// Use places and user http routes, but if there's a 404 error loading the routes, return an error message
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
@@ -29,6 +32,7 @@ app.use((req, res, next) => {
   throw error;
 });
 
+// Use places and user http routes, but if there's a 505 error loading the routes, return an error message
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
@@ -37,6 +41,7 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
+// Connect to the mongodb server, username: api_waphle. Pwd: apiwaphleBruv0706
 mongoose //connects mongoose under mongodb API
   .connect(
     `mongodb+srv://api_waphle:apiwaphleBruv0706@cluster0.acjvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
